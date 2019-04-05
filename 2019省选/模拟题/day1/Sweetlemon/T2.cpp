43105ef3157332daadf40d9128b0b7bb632cd54c
mrj1018 <mrj1018@163.com>
2019-04-05 23:41:50 +0800

    Update T2

--- 2019省选/模拟题/day1/Sweetlemon/T2.cpp
+++ 2019省选/模拟题/day1/Sweetlemon/T2.cpp
@@ -1,6 +1,17 @@
 #include <cstdio>
+#define MAXN 505
 using namespace std;
+int a[MAXN];
 int main(void){
-    
+    int test_datas;
+    scanf("%d",&test_datas);
+    for (int test_datai=1;test_datai<=test_datas;test_datai++){
+        printf("Case #%d: ",test_datai);
+        int n;
+        scanf("%d",&n);
+        for (int i=1;i<=n;i++)
+            scanf("%d",a+i);
+        
+    }
     return 0;
 }
